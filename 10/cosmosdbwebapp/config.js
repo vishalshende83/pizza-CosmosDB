9765c176a2005c365e0b416ef6c1b6fd3f623396
Vishal Shende <vishal.shende@gmail.com>
2023-02-20 18:40:20 -0500

    Updated

--- 10/cosmosdbwebapp/config.js
+++ 10/cosmosdbwebapp/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.endpoint = ''
-config.key = ''
+config.endpoint = 'https://vshende.documents.azure.com:443/'
+config.key = 'gtdNh9t3YBqJOS9Xrc0JaFoosqixJcUQzupGvfIX3Hgpk5NtG9noP8mfFAkamABi1KlLHNJ8VA3pACDbpmtzUg=='
 
 config.database = { id: 'pizzadb' }
 config.container = { id: 'pizzas' }
