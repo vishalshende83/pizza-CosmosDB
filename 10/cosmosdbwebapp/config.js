4335229c044294d8e6c638d95181aa1eadf68b39
Vishal Shende <vishal.shende@gmail.com>
2023-02-21 00:14:17 -0500

    Removed Password

--- 10/cosmosdbwebapp/config.js
+++ 10/cosmosdbwebapp/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.endpoint = 'https://vshende01.documents.azure.com:443/'
-config.key = 'TFev4UmItw2WW6lcPeZGxzbHcCblJ6URGAB3bbbNHrcYfbIV0JKhpNrCIEVFQppwkxNWC3dYKaLbACDbPKRIDQ=='
+config.endpoint = ''
+config.key = ''
 
 config.database = { id: 'pizzadb' }
 config.container = { id: 'pizzas' }
